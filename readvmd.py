030977a7a5a649a254f8526f625808dfabd48c3e
Ndgt <nadesubber2@gmail.com>
2024-12-06 23:30:00 +0900

    update source

--- readvmd.py
+++ readvmd.py
@@ -35,7 +35,7 @@ else:
         # Motion Data Count
         vmd_motion_count = VMD_MOTION_COUNT(Count=vmdread(file, 4, "I"))
         vmdoutput(result, "\n--- Motion Data Count ---")
-        vmdoutput(result, "Motion Data Count : " + str(vmd_motion_count.Count))
+        vmdoutput(result, str(vmd_motion_count.Count))
 
         # Motion Data
         vmdoutput(result, "\n--- Motion Data ---")
@@ -56,7 +56,7 @@ else:
         # Skin Data Count
         vmd_skin_count = VMD_SKIN_COUNT(Count=vmdread(file, 4, "I"))
         vmdoutput(result, "\n--- Skin Data Count ---")
-        vmdoutput(result, "Skin Data Count : " + str(vmd_skin_count.Count))
+        vmdoutput(result, str(vmd_skin_count.Count))
 
         # Skin Data
         vmdoutput(result, "\n--- Skin Data ---")
@@ -75,4 +75,60 @@ else:
         # Camera Data Count
         vmd_camera_count = VMD_CAMERA_COUNT(Count=vmdread(file, 4, "I"))
         vmdoutput(result, "\n--- Camera Data Count ---")
-        vmdoutput(result, "Camera Data Count : " + str(vmd_camera_count.Count))
+        vmdoutput(result, str(vmd_camera_count.Count))
+
+        # Camera Data
+        vmdoutput(result, "\n--- Camera Data ---")
+        for _count in range(vmd_camera_count.Count):
+            vmd_camera = VMD_CAMERA(
+                FrameNo=vmdread(file, 4, "I"),
+                Length=vmdread(file, 4, "f"),
+                Location=vmdread(file, 12, "3f"),
+                Rotation=vmdread(file, 12, "3f"),
+                Interpolation=vmdread(file, 24, "24B"),
+                ViewingAngle=vmdread(file, 4, "I"),
+                Perspective=vmdread(file, 1, "B")
+            )
+            for i in range(len(vmd_camera)):
+                vmdoutput(result, vmd_camera)
+                # vmdoutput(result, vmd_camera[i]) # output each data
+
+            vmdoutput(result, "")
+
+        # Light Data Count
+        vmd_light_count = VMD_LIGHT_COUNT(Count=vmdread(file, 4, "I"))
+        vmdoutput(result, "\n--- Light Data Count ---")
+        vmdoutput(result, str(vmd_light_count.Count))
+
+        # Light Data
+        vmdoutput(result, "\n--- Light Data ---")
+        for _count in range(vmd_light_count.Count):
+            vmd_light = VMD_LIGHT(
+                FrameNo=vmdread(file, 4, "I"),
+                RGB=vmdread(file, 12, "3f"),
+                Location=vmdread(file, 12, "3f"),
+            )
+            for i in range(len(vmd_light)):
+                vmdoutput(result, vmd_light)
+                # vmdoutput(result, vmd_light[i]) # output each data
+
+            vmdoutput(result, "")
+
+        # Self Shadow Data Count
+        vmd_self_shadow_count = VMD_SELF_SHADOW_COUNT(Count=vmdread(file, 4, "I"))
+        vmdoutput(result, "\n--- Self Shadow Data Count ---")
+        vmdoutput(result, str(vmd_self_shadow_count.Count))
+
+        # Self Shadow Data
+        vmdoutput(result, "\n--- Self Shadow Data ---")
+        for _count in range(vmd_self_shadow_count.Count):
+            vmd_self_shadow = VMD_SELF_SHADOW(
+                FrameNo=vmdread(file, 4, "I"),
+                Mode=vmdread(file, 1, "B"),
+                Distance=vmdread(file, 4, "f")
+            )
+            for i in range(len(vmd_self_shadow)):
+                vmdoutput(result, vmd_self_shadow)
+                # vmdoutput(result, vmd_self_shadow[i]) # output each data
+
+            vmdoutput(result, "")
